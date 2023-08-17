028b355b40b33bd975608f82a1be14b8bf40363a
eunsun <iso9864@naver.com>
2023-08-17 21:13:50 +0900

    first commit

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PostsPage() {
+    return (
+        <div></div>
+    );
+}
+
+export default PostsPage;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <>
+        <p className="bg-slate-900 text-white py-2 text-sm text-center">{"Don't forget to CODE your DREAM | All Right Reserved"}</p>
+         {/*<p>Don&apos;t</p> ' === &apos; */}
+        </>
+    );
+}
+
+export default Footer;
