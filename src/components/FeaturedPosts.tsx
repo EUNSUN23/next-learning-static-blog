0f553f3292ff816b9b8a2a01dba80bd8aa86868c
eunsun <iso9864@naver.com>
2023-08-17 21:34:50 +0900

    [HomePage] FeaturedPosts 컴포넌트 추가 (PostsGrid추가)

---
+++ src/components/FeaturedPosts.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostsGrid from "@/components/PostsGrid";
+
+function FeaturedPosts() {
+    return (
+        <section>
+            <h2>Featured Posts</h2>
+            <PostsGrid/>
+        </section>
+    );
+}
+
+export default FeaturedPosts;
