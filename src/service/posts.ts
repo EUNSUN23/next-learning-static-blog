bbc8a8d1bc2f82a79adb129623b98d31cdf9022a
eunsun <iso9864@naver.com>
2023-08-17 21:51:32 +0900

    [HomePage] post데이터 조회 로직 : getAllPosts -> getFeaturedPosts

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -11,6 +11,10 @@ export type Post = {
 }
 
 export async function getFeaturedPosts():Promise<Post[]>{
+    return getAllPosts()
+        .then((posts) => posts.filter((post) => post.featured));
+}
+export async function getAllPosts():Promise<Post[]>{
     const filePath = path.join(process.cwd(), 'data', 'posts.json');
     return readFile(filePath,'utf-8')
         .then<Post[]>(JSON.parse)
