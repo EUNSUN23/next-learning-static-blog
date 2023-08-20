21f86ac0b2e1122aa7d51430a921651dfdf12a66
eunsun <iso9864@naver.com>
2023-08-21 06:40:28 +0900

    [Post] 1. posts > post상세 > 마크다운 > @tailwindcss/typhograpyh 플러그인 설치 & prose 사용

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 
+// prose : tailwind.css를 사용하면 기본 css는 모두 reset되는데, @tailwindcss/typhograpy플러그인을 설치하고
+// prose키워드를 사용하면 기본 css가 적용되게 할 수 있다.
 function MarkdownViewer({content}:{content:string}) {
     return (
-        <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        <ReactMarkdown className='prose lg:prose-xl' remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
     );
 }
 
