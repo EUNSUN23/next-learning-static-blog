b6a904b662df5ed4f1e70f0ab19f971560f48c08
eunsun <iso9864@naver.com>
2023-08-22 05:48:53 +0900

    [Post] 1. posts > post상세 > 마크다운 > 마크다운 상세작업 / react-icons추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 import {getPostData} from "@/service/posts";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import MarkdownViewer from "@/components/MarkdownViewer";
+import {AiTwotoneCalendar} from "react-icons/ai";
 
 type Props = {
     params:{
@@ -10,15 +12,32 @@ type Props = {
     }
 }
 
+/* tailwind.css
+*  h-1/5 max-h-[500px] : 기본 높이는 전체 브라우저 높이의 1/5이지만 높이가 500px을 넘어가면 더이상 증가하지 않는다.
+* */
 async function PostPage({params}:Props) {
-    const post = await getPostData(params.slug);
+    const {title,description,date, path, content} = await getPostData(params.slug);
 
-    if(!post) return null;
     return (
-        <>
-            <h1>{post.title}</h1>
-            <MarkdownViewer content={post.content}/>
-        </>
+        <article className='rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4'>
+            <Image
+                className='w-full h-1/5 max-h-[500px]'
+                src={`/images/posts/${path}.png`}
+                alt={title}
+                width={760}
+                height={420}
+            />
+            <section className='flex flex-col p-4'>
+                <div className='flex items-center self-end text-sky-600'>
+                    <AiTwotoneCalendar/>
+                    <p className='font-semibold ml-2'>{date.toString()}</p>
+                </div>
+                <h1 className='text-4xl font-bold'>{title}</h1>
+                <p className='text-xl font-bold'>{description}</p>
+                <div className='w-44 border-2 border-sky-600 mt-4 mb-8'></div>
+                <MarkdownViewer content={content}/>
+            </section>
+        </article>
     );
 }
 
