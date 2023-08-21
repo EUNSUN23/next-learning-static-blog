2b6d913a93c50373fc6c4e52cab49626fe297789
eunsun <iso9864@naver.com>
2023-08-22 06:24:34 +0900

    [Post] 1. posts > post상세 > 마크다운 > 마크다운 상세작업 / SyntaxHighlighter 적용

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -35,7 +35,9 @@ async function PostPage({params}:Props) {
                 <h1 className='text-4xl font-bold'>{title}</h1>
                 <p className='text-xl font-bold'>{description}</p>
                 <div className='w-44 border-2 border-sky-600 mt-4 mb-8'></div>
-                <MarkdownViewer content={content}/>
+                <MarkdownViewer
+                    content={content}
+                />
             </section>
         </article>
     );
--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/cjs/prism";
+import {materialDark} from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 // prose : tailwind.css를 사용하면 기본 css는 모두 reset되는데, @tailwindcss/typhograpy플러그인을 설치하고
 // prose키워드를 사용하면 기본 css가 적용되게 할 수 있다.
+/* react-markdown
+*  - components : 마크다운 태그에 대한 상세 설정 할수있는 속성
+*
+* */
 function MarkdownViewer({content}:{content:string}) {
     return (
-        <ReactMarkdown className='prose lg:prose-xl' remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+        <ReactMarkdown
+            className='prose lg:prose-xl'
+            remarkPlugins={[remarkGfm]}
+            components={{
+                code({node, inline, className, children, ...props}) {
+                    const match = /language-(\w+)/.exec(className || '')
+                    return !inline && match ? (
+                        <SyntaxHighlighter
+                            language={match[1]}
+                            PreTag="div"
+                            {...props}
+                            style={materialDark}
+                        >
+                            {String(children).replace(/\n$/, '')}
+                        </SyntaxHighlighter>
+                    ) : (
+                        <code {...props} className={className}>
+                            {children}
+                        </code>
+                    )
+                }
+            }}
+
+        >{content}</ReactMarkdown>
     );
 }
 
