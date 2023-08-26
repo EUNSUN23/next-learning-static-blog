5c9918d0d1da4e6d2dfef4e46fd5861c15ca7a69
eunsun <iso9864@naver.com>
2023-08-26 17:49:31 +0900

    [Contact] 이메일 전송 구현
    1. @types/nodemailer설치 : npm i --save-dev @types/nodemailer
    
    2. email.ts에 nodemailer사용한 이메일 전송 로직 추가

--- src/service/email.ts
+++ src/service/email.ts
@@ -1,5 +1,33 @@
+import nodemailer from 'nodemailer';
+
 export type EmailData = {
-    from:string;
-    subject:string;
-    message:string;
+    from: string;
+    subject: string;
+    message: string;
+}
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: { // .env.local 파일(보안상 깃헙에 올라가지 않음)에 저장된 user, password(구글 계정에 설정한 App password) 입력
+        user: process.env.AUTH_USER,
+        pass: process.env.AUTH_PASS
+    }
+});
+
+export async function sendEmail({subject, from, message}: EmailData) {
+    const mailData = {
+        to: process.env.AUTH_PASS,
+        subject: `[BLOG] ${subject}`,
+        from,
+        html: `
+            <h1>${subject}</h1>
+            <div>${message}</div>
+            <br/>
+            <p>보낸사람: ${from}</p>
+            `
+    };
+
+    return transporter.sendMail(mailData);
 }
